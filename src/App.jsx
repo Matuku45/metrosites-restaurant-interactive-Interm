68f8f1485cc3d41088e8a10263e1882c3ce54e69
Thabiso Mapoulo <manuelthabisomapoulo@gmail.com>
2025-10-28 22:25:07 +0200

    defined the Router

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Offers from "./pages/Offers";
 import Events from "./pages/Events";
 import Gallery from "./pages/gallery";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -31,6 +32,7 @@ function App() {
             <Route path="/events" element={<Events />} />
             <Route path="/gallery" element={<Gallery />} />
             <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
 
